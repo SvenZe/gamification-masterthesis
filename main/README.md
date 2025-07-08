e77b2e110adf8e964a2e55c6bcbd5183cbd80bf2
SvenZe <73070732+SvenZe@users.noreply.github.com>
2025-07-08 13:16:24 +0000

    Update 1

--- main/README.md
+++
@@ -1,2 +0,0 @@
-# gamification-masterthesis
-Prototype of the master thesis Development of a gamification approach for ESG reporting
--- main/vite.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite';
-
-export default defineConfig({
-  root: 'public', // Setzt das 'public' Verzeichnis als Root für Vite
-  build: {
-    outDir: '../dist', // Ausgabeordner für den Build (Standard ist 'dist')
-  },
-});
